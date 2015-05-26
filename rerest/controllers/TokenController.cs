d30c499bab2f7c8eb0ed9dd6e1462e64a484ac42
Kasper Guldmann Nielsen <git@kalleguld.dk>
2015-05-26 19:54:39 +0200

    removed hopefully useless projects

---
+++ rerest/controllers/TokenController.cs
@@ -0,0 +1,43 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using rerest.models.output;
+using rerest.models.output.exceptions;
+
+namespace rerest.controllers
+{
+    [ServiceContract]
+    class TokenController : BaseController
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "?username={username}&password={password}")]
+        public TokenInfo CreateToken(string username, string password)
+        {
+            CheckNull(username, "username");
+            CheckNull(password, "password");
+            using (var connection = GetConnection())
+            {
+                var token = connection.UserService.CreateToken(username, password);
+                if (token == null) throw new JsonLoginError();
+                connection.SaveChanges();
+                return new TokenInfo(token);
+            }
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", 
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "{guidStr}/")]
+        public TokenInfo GetToken(string guidStr)
+        {
+            using (var connection = GetConnection())
+            {
+                var token = GetToken(connection, guidStr);
+                return new TokenInfo(token);
+            }
+        }
+
+    }
+}
---
+++ rerest/models/output/MessageList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using modelInterface;
+
+namespace rerest.models.output
+{
+    [DataContract]
+    public class MessageList : JsonResponse
+    {
+        [DataMember]
+        public IList<int> Messages { get; set; }
+
+        public MessageList() { Messages = new List<int>(); }
+
+        public MessageList(IEnumerable<IMessage> messages)
+        {
+            var ids = messages.Select(m => m.Id);
+            Messages = new List<int>(ids);
+        }
+    }
+}
